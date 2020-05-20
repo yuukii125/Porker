80f47ac102eb1498f7343a636169b9bbd24ed17b
yuukii125 <y.kipro125@gmail.com>
2020-05-20 21:37:26 +0900

    手札配布コード変更

--- porker.rb
+++ porker.rb
@@ -27,17 +27,15 @@ def first_message
 end
 
 #手札を配布
-def input_hand(name)
-  # 1.1-13と♡♢♤♧の組み合わせでトランプ52枚
+def input_hand
+  # トランプ52枚
   cards = ["♥", "♠", "♦", "♣"].product(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]).map(&:join)
-  # 2.手札をランダムで5枚配る
-  cards = cards.sample(5)  
 
-  if name == "CP"
-    puts "#{name}の手札は.....です\n\n"
-  else
-    puts "#{name}の手札は#{cards}です\n\n"
-  end
+  cards = cards.sample(10)   # 手札をランダムで5枚配る 
+  cp_cards = cards.first(5)
+  you_cards = cards.last(5)
+
+  return cp_cards, you_cards
   sleep(0.5)
 end
 
@@ -50,38 +48,61 @@ def number_select
     print "無効な値です。"
     return number_select
   end
+
+  return num_select
 end
+
+# 6.役判定 役に番号つけて番号で勝敗きめる
+# def vs_judgement("name")
+  # if    #ロイヤルフラッシュ 1
+    
+  # elsif   #ストレートフラッシュ 2
+  # elsif   #4カード  3
+  # elsif   #フルハウス 4
+  # elsif   # 5
+  # elsif   #6
+  # elsif   #7
+  # elsif   #8
+  # else   #9
+    
+  # end
+  
+  # if    #CPの役番号が強い
+  #   judgement = "You lose..."
+    
+  # elsif     #CPの役番号が弱い
+  #   judgement = "You Win!!"
+    
+  # else     #CPの役番号と同じ
+  #   judgement = "draw"
+    
+  # end
+    # return vs_judge
+# end
 
 
 # 3.勝負選択
-def game_selection(cp_hand_array, you_hand_array)
+def game_selection(name, cp_hand_array, you_hand_array)
   
-  game_select = number_select
+  puts "#{name}の手札は#{you_hand_array}です\n\n"
 
+  game_select = number_select
+  
   if game_select == 3  # 4.降りたら負け
-    puts "You lose...\n\n"
-    
+    puts "CPの手札は#{cp_hand_array}でした"
+    judgement = "You lose..."
   elsif game_select == 1  # 5.勝負を選択
     puts "CPの手札は#{cp_hand_array}です"
     puts "あなたの手札は#{you_hand_array}です"
+    judgement = vs_judge
   end
-
+  
+  puts "#{judgement}\n\n"  # 7.勝敗表示
 end
 
 
-
-
-# 6.役判定
-# 7.勝敗表示
-
-
-
-
 #出力開始
-first_message
+first_message  #start
+cp_hand, you_hand = input_hand  #手札配布
 
-cp_hand = input_hand("CP")
-
-you_hand = input_hand("あなた")
-
-game_selection(cp_hand, you_hand)
+game_selection("あなた", cp_hand, you_hand)  #ゲーム開始
