5fa5b18603527726c9d638cd53327db675dea7e4
yuukii125 <y.kipro125@gmail.com>
2020-05-21 23:11:37 +0900

    役判定

--- porker.rb
+++ porker.rb
@@ -20,6 +20,8 @@ def first_message
     7.3カード
     8.2ペア
     9.1ペア
+
+    A->1, X->10, J->11, Q-> 12, K->13
   ----------------------------------\n\n
   TEXT
   
@@ -29,13 +31,16 @@ end
 #手札を配布
 def input_hand
   # トランプ52枚
-  cards = ["♥", "♠", "♦", "♣"].product(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]).map(&:join)
+  cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "X", "J", "Q", "K"].product(["♥", "♠", "♦", "♣"]).map(&:join)
 
   cards = cards.sample(10)   # 手札をランダムで5枚配る 
   cp_cards = cards.first(5)
   you_cards = cards.last(5)
 
-  return cp_cards, you_cards
+  cp_hand = cp_cards.sort_by{|a| a.to_i }  #手札を昇順に
+  you_hand = you_cards.sort_by{|a| a.to_i }
+
+  return cp_hand, you_hand
   sleep(0.5)
 end
 
@@ -53,39 +58,55 @@ def number_select
 end
 
 # 6.役判定 役に番号つけて番号で勝敗きめる
-# def vs_judgement("name")
-  # if    #ロイヤルフラッシュ 1
-    
-  # elsif   #ストレートフラッシュ 2
-  # elsif   #4カード  3
-  # elsif   #フルハウス 4
-  # elsif   # 5
-  # elsif   #6
-  # elsif   #7
-  # elsif   #8
-  # else   #9
-    
-  # end
+def vs_judgement(hand_array)
+  suits = []
+  numbers = []
+
+  hand_array.each do |c|  #番号とマークを分ける
+    numbers.push c[0]
+    suits.push c[1]
+  end
+
+#   if  suits.group_by(&:itself).transform_values(&:size).count == 1 &&  #ロイヤルフラッシュ 1
+#     judge_num = 1
+
+  # elsif suits.group_by(&:itself).transform_values(&:size).count == 1 &&  #ストレートフラッシュ 2
+#   #   judge_num = 2
+
+  if numbers.group_by(&:itself).transform_values(&:size).value?(4)  #4カード  3
+    judge_num = 3
+
+#   # elsif numbers.uniq.size   #フルハウス 4
+#   #   judge_num = 4
+
+  elsif suits.group_by(&:itself).transform_values(&:size).count == 1  #フラッシュ 5
+    judge_num = 5
+
+#   elsif numbers.uniq.size = 2  #ストレート6
+#     judge_num = 6
+
+  elsif numbers.group_by(&:itself).transform_values(&:size).value?(3)   #3カード 7
+    judge_num = 7
+
+  elsif numbers.group_by(&:itself).transform_values(&:size).count == 3 && numbers.group_by(&:itself).transform_values(&:size).value?(3)  #2ペア 8
+    judge_num = 8
+
+  elsif numbers.group_by(&:itself).transform_values(&:size).count == 4  #1ペア 9
+    judge_num = 9
+
+  else  #何も揃っていない
+    judge_num = 10
+  end
+
+  return judge_num
+end
   
-  # if    #CPの役番号が強い
-  #   judgement = "You lose..."
-    
-  # elsif     #CPの役番号が弱い
-  #   judgement = "You Win!!"
-    
-  # else     #CPの役番号と同じ
-  #   judgement = "draw"
-    
-  # end
-    # return vs_judge
-# end
-
-
+  
 # 3.勝負選択
-def game_selection(name, cp_hand_array, you_hand_array)
+def game_selection(name, cp_hand_array, you_hand_array, cp_judge_num, you_judge_num)
   
   puts "#{name}の手札は#{you_hand_array}です\n\n"
-
+  
   game_select = number_select
   
   if game_select == 3  # 4.降りたら負け
@@ -94,15 +115,32 @@ def game_selection(name, cp_hand_array, you_hand_array)
   elsif game_select == 1  # 5.勝負を選択
     puts "CPの手札は#{cp_hand_array}です"
     puts "あなたの手札は#{you_hand_array}です"
-    judgement = vs_judge
+
+    if cp_judge_num < you_judge_num   #CPの役番号が強い
+      judgement = "You lose..."
+
+    elsif cp_judge_num == you_judge_num    #CPの役番号と同じ
+      judgement = "draw"
+
+    else                               #CPの役番号が弱い
+      judgement = "You Win!!"
+
+    end
   end
   
-  puts "#{judgement}\n\n"  # 7.勝敗表示
+  puts "\n#{judgement}\n\n"  # 7.勝敗表示
 end
 
 
 #出力開始
-first_message  #start
-cp_hand, you_hand = input_hand  #手札配布
+# first_message  #start
 
-game_selection("あなた", cp_hand, you_hand)  #ゲーム開始
+cp_hands, you_hands = input_hand  #手札配布
+
+cp_judge = vs_judgement(cp_hands)
+you_judge = vs_judgement(you_hands)
+
+# p "#{vs_judgement(cp_hands)} CP"
+# p "#{vs_judgement(you_hands)} you"
+
+game_selection("あなた", cp_hands, you_hands,cp_judge, you_judge)  #ゲーム開始
